4567c456d2700d27f2eb3de0c1f020d01de4d04e
Anu Sharma <anulepau@gmail.com>
2022-05-16 18:05:29 -0700

    updated notes

--- src/lib/GraphView/index.svelte
+++ src/lib/GraphView/index.svelte
@@ -24,6 +24,18 @@
 	object of our state from the store  (+ types object/schema): for nodes & edges
 	ZoomPane: all info for behavior and handler for d3
 
+	We set up a store that handles the state objects. Those objects will be created in the nodes and edges components. 
+	The objects themselves will have initial coordinates, updated coordinates and all the types and interface data.
+	In the store, we have our subscription functionality set up. 
+	Everything passed to the store will be updated through the functionality in the store (just like redux) and that way we have access to it within all components
+	D3 Functionality will live in the Store as well since D3 is 100% Data driven
+
+	1. create a store file and list out type schemas and the necessary functionalities needed for nodes, edges and zoompane
+	2. be sure to map out which variables need to be accessed by which functions and passed in which component 
+	3. carefully render components in a way that can test the usability of the Svelvet library once we render Svelvet in the router folder's index.js
+
+	go through node store in react to translate it over to svelte 
+
 	*/
 
 </script>
