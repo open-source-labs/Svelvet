884e1c761ab1a53e1a86ed36aaba53759dd5256c
Aaron <awillettnyc@gmail.com>
2022-05-31 12:51:41 -0400

    yml file reconfig

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,7 +9,7 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter(),
+		adapter: adapter()
 		// vite: {
 		// 	server: {
 		// 		watch: {
