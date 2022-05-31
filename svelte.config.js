0edc667eb02b071b0a804f20d68e09e6a9d135c4
Andrew Widjaja <andrewdwidjaja@gmail.com>
2022-05-31 11:33:23 -0700

    config for deployment on AWS

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,7 +9,7 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter(),
+		adapter: adapter()
 		// vite: {
 		// 	server: {
 		// 		watch: {
