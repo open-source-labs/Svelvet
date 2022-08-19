25a256b3740c8de15d29b30d5bf65d22fa117342
Jefffrey Wentworth <jeffreyjwentworth@gmail.com>
2022-08-18 20:11:33 -0400

    added conditional rendering of modal on button click - working out details

--- src/repl/CodeMirror.svelte
+++ src/repl/CodeMirror.svelte
@@ -416,7 +416,7 @@
       textColor: "${textColor}"
     },`;
     editor.setValue(code || editStrP1 + newNode + editStrP2);
-    inputToggle = false;
+    // inputToggle = false;
   };
 
   const editStrP1 = `<script>
@@ -500,6 +500,28 @@ const initialEdges = [
 </script>
 <button on:click={handleNewNode}> NEW NODE!!! </button>
 
+{#if inputToggle === true}
+<div id="myModal" class="modal">
+  <div class="modal-content">
+    <div class="modal-header">
+      <span class="close">&times;</span>
+      <h2>Modal Header</h2>
+    </div>
+    <div class="modal-body">
+      <input type="text">
+      <input type="text">
+      <input type="text">
+      <input type="text">
+      <input type="text">
+      <input type="text">
+    </div>
+    <div class="modal-footer">
+      <h3>Modal Footer</h3>
+    </div>
+  </div>
+</div>
+{/if}
+
 <div class="codemirror-container" bind:offsetWidth={w} bind:offsetHeight={h}>
   <textarea bind:this={refs.editor} readonly value={code} />
 
@@ -561,5 +583,50 @@ const initialEdges = [
     color: #ccc;
     tab-size: 2;
     -moz-tab-size: 2;
+  }
+
+  /* Modal Header */
+  .modal-header {
+    padding: 2px 16px;
+    background-color: #5cb85c;
+    color: white;
+  }
+
+  /* Modal Body */
+  .modal-body {
+    padding: 2px 16px;
+    opacity: 100;
+  }
+
+  /* Modal Footer */
+  .modal-footer {
+    padding: 2px 16px;
+    background-color: #5cb85c;
+    color: white;
+  }
+
+  /* Modal Content */
+  .modal-content {
+    position: fixed;
+    background-color: #5cb85c;
+    margin: 15% auto;
+    padding: 1rem;
+    border: 1px solid #888;
+    max-width: 40vw;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+    animation-name: animatetop;
+    animation-duration: 0.4s;
+  }
+
+  /* Add Animation */
+  @keyframes animatetop {
+    from {
+      top: -300px;
+      opacity: 0;
+    }
+    to {
+      top: 0;
+      opacity: 20;
+    }
   }
 </style>
