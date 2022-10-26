6ee0fe80562b499dc96ba1940bce8e1b0f918a33
Ian <ianhnizdo@gmail.com>
2022-10-25 20:10:02 -0400

    changes saved before merge

--- src/routes/docs/CSS-Background.svelte
+++ src/routes/docs/CSS-Background.svelte
@@ -14,12 +14,23 @@
 
 <h3 class="text-xl font-semibold mt-12">Background Color</h3>
 <iframe
-  src="https://codesandbox.io/embed/custom-nodes-gl35z6?autoresize=1&codemirror=1&fontsize=14&hidenavigation=1&module=%2FApp.svelte&theme=dark&view=split&editorsize=50"
-  
-  style="width:100%; height:500px; border:0; border-radius: 8px; overflow:hidden;"
-  title="custom-nodes"
-  allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
-  sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
+src = "https://codesandbox.io/embed/css-backgrounds-5mw3bk?file=/App.svelte?autoresize=1&codemirror=1&fontsize=14&hidenavigation=1&module=%2FApp.svelte&theme=dark&view=split&editorsize=50"
+
+style="width:100%; height:500px; border:0; border-radius: 8px; overflow:hidden;"
+title="custom-nodes"
+allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
+sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
 />
+
+<div>
+    
+    <p class="my-4 text-gray-600">
+        
+        Look at the bottom with the Svelvet declaration and you'll see a property called bgColor set equal to 'pink'. To make the background whatever CSS color you want you merely need to just put in your new string whether it be hexadecimal or string colors.
+        
+    </p>
+    
+</div>
+
 
 </div>
