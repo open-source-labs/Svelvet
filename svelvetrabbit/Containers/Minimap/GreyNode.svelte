b9f04e085627ae2b405b883aeaadae646ea33a0a
Taylor Alan <tay10alan60@gmail.com>
2022-12-14 09:27:32 -0800

    transferance of files from zoomBr

---
+++ svelvetrabbit/Containers/Minimap/GreyNode.svelte
@@ -0,0 +1,14 @@
+<script>
+    export let key
+    export let node
+    
+    //console.log(node)
+    </script>
+     
+      
+      <style>
+        .GreyNode {
+            pointer-events: none;
+        }
+      </style>
+      
