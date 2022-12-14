3561195ad30ee7734d840b1c84e95cf5b5bf35c3
Timmy Lem <timmylem@trams-mbp.lan>
2022-12-13 17:12:10 -0800

    added functionality that allows custom svelte components to be loaded as nodes co-authored by: Tony Lim, Emma Ferguson, Dillon McKenna, Taylor Alan, Timmy Lem

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,14 @@
 	<script>
 	import Svelvet from "../svelvetrabbit";
+	import BlueThing from "./BlueThing.svelte";
 
 		const initialNodes = [
-			
 		  {
 		    id: 1,
 		    position: { x: 150, y: 50 },
-		    data: { label: "default styling" },
+		    data: {custom: BlueThing},
 		    width: 150,
-		    height: 40,
+		    height: 200,
 		    bgColor: "white"
 		  },
 		  {
@@ -104,7 +104,7 @@
 <main>
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-	<Svelvet  nodes={initialNodes} width={800} height={800} initialZoom={10} initialLocation={{x:1000, y:400}} edges={initialEdges} bgColor={'pink'} background/>
+	<Svelvet nodes={initialNodes} width={800} height={800} initialZoom={10} initialLocation={{x:1000, y:400}} edges={initialEdges} bgColor={'pink'} background/>
 </main>
 
 <style>
