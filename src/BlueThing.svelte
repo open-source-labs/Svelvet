50c7ec95993970de54950dba11df061c7582e2c1
Timmy Lem <timmylem@trams-mbp.lan>
2022-12-14 10:13:33 -0800

    test component files co-authored by: Timmy, Tony, Emma, Dillon, Taylor

---
+++ src/BlueThing.svelte
@@ -0,0 +1,17 @@
+<script>
+	import RedThing from './RedThing.svelte';
+</script>
+
+
+<div>
+	<strong>Blue thing1</strong>
+</div>
+<strong>Blue thing2</strong>
+<button>Blue thing3</button>
+<RedThing/>
+
+<style>
+	strong {
+		color: blue;
+	}
+</style>
