9c28f1043d144cb9eaaf6c8831db46da167d4004
Tony Lim <tonylim9438@gmail.com>
2022-12-15 15:28:32 -0800

    created-reactive-login-for-demo

--- src/BlueThing.svelte
+++ src/BlueThing.svelte
@@ -1,48 +1,31 @@
 <!-- This is the HTML for your svelte component -->
-<div class='page'>
-	<nav>
-  <ul>
-    <li><a href="#">Home</a></li>
-    <li><a href="#">About</a></li>
-    <li><a href="#">Contact</a></li>
-  </ul>
-</nav>
-<h1>My Basic Webpage</h1>
-<p>This is a basic webpage created with svelte.</p>
+<script>
+let user = {loggedIn: false}
+function toggle () {
+  user.loggedIn = !user.loggedIn
+}
+</script>
+
+
+{#if !user.loggedIn}
+<div class='login'>
+  <input type="text" placeholder="Username" />
+  <input type="password" placeholder="Password" />
+  <button on:click={toggle}> Login Here </button>
 </div>
+{:else}
+ <div> 
+  You are logged in 
+  <button on:click={toggle}> Logout </button>
+ </div>
+{/if}
 
 <!-- This is the CSS for your svelte component -->
 <style>
-	.page {
+	.login {
 		display: flex;
 		flex-direction: column;
 		height: 300px;
 		width: 300px;
 	}
-	nav {
-		background-color: #333;
-  }
-  ul {
-    list-style-type: none;
-    margin: 0;
-    padding: 0;
-    overflow: hidden;
-  }
-  li {
-    float: left;
-  }
-  li a {
-    display: block;
-    color: white;
-    text-align: center;
-    padding: 14px 16px;
-    text-decoration: none;
-  }
-  li a:hover {
-    background-color: #111;
-  }
-  h1 {
-    color: blue;
-  }
-	
 </style>
