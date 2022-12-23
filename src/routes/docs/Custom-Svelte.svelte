2d95d3e9650cc4efae49715c6cec53907cbe02fb
Timmy Lem <timmylem@trams-mbp.lan>
2022-12-23 15:11:17 -0800

    Updated code sand box URLs for doc pages to be at most updated version of Svelte co-authored by: Timmy, Tony, Dillon, Emma, Taylor

---
+++ src/routes/docs/Custom-Svelte.svelte
@@ -0,0 +1,21 @@
+<script>
+
+</script>
+
+<h2 class="text-4xl font-semibold mb-12">Custom Svelte Components</h2>
+
+<p class="text-gray-600">
+  <code class="bg-rose-100 rounded-lg p-1">Svelvet</code> allows you to customize your nodes to have custom Svelte components as nodes!
+  To access this feature, simply change the <code class="bg-rose-100 rounded-lg p-1">data</code> object's <code class="bg-rose-100 rounded-lg p-1">key</code> to <code class="bg-rose-100 rounded-lg p-1">custom</code> and <code class="bg-rose-100 rounded-lg p-1">value</code> to the name of the imported Svelte component.
+</p>
+
+<div>
+  <h3 class="text-xl font-semibold mt-12 mb-6">Implementing Custom Svelte Components</h3>
+</div>
+
+<iframe src="https://codesandbox.io/embed/custom-svelte-components-p4ztve?autoresize=1&fontsize=14&hidenavigation=1&module=%2FApp.svelte&theme=dark"
+     style="width:100%; height:500px; border:0; border-radius: 4px; overflow:hidden;"
+     title="Custom Svelte Components"
+     allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
+     sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
+></iframe>
--- src/routes/docs/custom-nodes.svelte
+++ src/routes/docs/custom-nodes.svelte
@@ -64,10 +64,9 @@
 
 <h3 class="text-xl font-semibold mt-12 mb-6">Implementing Custom Nodes</h3>
 
-<iframe
-  src="https://codesandbox.io/embed/custom-nodes-gl35z6?autoresize=1&codemirror=1&fontsize=14&hidenavigation=1&module=%2FApp.svelte&theme=dark&view=split&editorsize=50"
-  style="width:100%; height:500px; border:0; border-radius: 8px; overflow:hidden;"
-  title="custom-nodes"
-  allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
-  sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
-/>
+<iframe src="https://codesandbox.io/embed/custom-nodes-forked-1ml2fb?autoresize=1&fontsize=14&hidenavigation=1&module=%2FApp.svelte&theme=dark"
+     style="width:100%; height:500px; border:0; border-radius: 4px; overflow:hidden;"
+     title="custom-nodes (forked)"
+     allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
+     sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
+></iframe>
