5c11b4a0449a29671cedb5082a12b54b96c11c40
Tony Lim <tonylim9438@gmail.com>
2022-12-23 16:53:12 -0800

    mini-map documentation finished Co-authored-by: Dillon McKenna, Taylor Alan, Emma Ferguson, Timmmy Lem, Tony Lim

--- src/routes/docs/Minimap.svelte
+++ src/routes/docs/Minimap.svelte
@@ -5,7 +5,7 @@
     <code class="code">Svelvet 5.0</code> introduces a new optional feature that can be added to diagrams now. To access this feature set the minimap prop to <code class="code">{true}</code> in your svelvet application. 
 	</p>
 	<p>
-	The node featuring dolphins contains a video tag, and the node labeled: Message Box contains a simple form input. The node featuring "ragtime-southernmelody" contains an embedded audio player from Audio Mack. Last but not least, the node in the lower right hand listing common beverages contains a simple unordered list.
+	Paragraph here
   </p>
   <div class="overflow-hidden my-8 border border-gray-200 rounded-lg shadow-lg shadow-slate-100">
 	<!-- <Svelvet 
@@ -17,20 +17,20 @@
 
   </div>
   <p class="my-4 text-gray-600">
-     To render HTML elements inside your node, include the following inside your Svelvet component declaration. 
+    It is important to note that the minimap scales to your current zoom settings which you can affect by scrolling your mouse wheel up or down; if you are on a touchpad or touchscreen, expand or collapse and it should work the same way. Different browsers may have different settings. 
 	 
 	 </p>
-	 <p>We will be using the <code class="code">data</code> prop with a key of <code class="code">html</code>. The value for the  <code class="code">html</code> key will be your raw HTML elements surrounded by backticks (``). The declaration for the node featuring dolphins has been included below to serve as an example. <br><br> 
-     <!-- <code> <Svelvet nodes={initialNodes} edges={initialEdges} background snap={true} snapTo={30}/> </code>  -->
-
-     <!-- <code class="block rounded-md p-3 bg-rose-100 text-rose-600"> 
-		{code_sample0} <br> {code_sample1}	<br>  {code_sample2} <br>  {code_sample3} <br> 
-		{code_sample4} <br> {code_sample5} <br> {code_sample6} <br> {code_sample7}
-    </code> -->
-
-     <br> Let your imagination run wild with this feature!
+	 <p>Create nodes interactively by clicking and dragging an anchor to an empty space and see how the minimap interacts with nodes- far away, close by, small or large. <br> You can also create nodes by simply following the format in Codesandbox if you want to interact with the Svelvet component directly. <br> 
+    <br> Let your imagination run wild with this feature!
   </p>
 </div>
+
+<iframe src="https://codesandbox.io/embed/minimap-website-documentation-9uy92c?autoresize=1&fontsize=14&module=%2FApp.svelte&theme=dark"
+     style="width:100%; height:500px; border:0; border-radius: 4px; overflow:hidden;"
+     title="Custom Svelte Components"
+     allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
+     sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
+></iframe>
 
 <style>
     
