2fe228ed1f23a6bf6bfc71ce5de67dbe7c9d91cc
rachelheplus <hejia0917rachel@gmail.com>
2023-01-18 21:16:23 -0500

    Refactoring store still in progress

---
+++ src/lib/Containers/DevTools/index.svelte
@@ -0,0 +1,34 @@
+<script>
+  // Declaring variables for Svelvet components which will be usable in other files
+  import { Edge, Anchor, Node, createStore } from '$lib/stores/storeSchema'
+    import { onMount } from 'svelte';
+
+  export let nodes;
+  export let edges;
+  console.log('This should be the array as initialNodes: ', nodes);
+  console.log('This should be the array as initialEdges: ', edges);
+
+  const testingStore = createStore();
+  const { nodesStore, edgesStore, anchorsStore } = testingStore;
+
+  onMount(() => {
+        const arrNodes = nodes.map(node => {
+            return {
+                id: node.id,
+                width: node.width,
+                height: node.height,
+                bgColor: node.bgColor,
+                data: JSON.stringify(node.data),
+                positionX: node.position.x,
+                positionY: node.position.y
+            }
+        })
+      testingStore.nodesStore.set(arrNodes);
+  })
+</script>
+
+
+<h1>Hello World!</h1>
+<div>
+    {JSON.stringify($nodesStore)}
+</div>
---
+++ src/lib/stores/storeSchema.ts
@@ -0,0 +1,138 @@
+import { writable, derived, get, readable } from 'svelte/store';
+import type { Readable, Writable } from 'svelte/store';
+// import type { Node, Edge } from '../types/types';
+/**
+ *  When a user install svelvet package into their repo and import Svelvet from Svelvet,
+ *  To create a node, the user needs to provide the following information:
+ *  [{id, position {x, y}, data, width, height, bgColor}, {}] all required
+ *  The position x, y is for the left top corner of the node
+ * 
+ * 
+ *  To create an edge, the user needs to provide the following information:
+ *  {id, source, target} required
+ *  {label, labelTextColor, labelBgColor, animate, type, arrow, edgeColor} optional
+ *  For now, only focus on the required information for edges
+ * 
+ * 
+ *  1. We do not want to change the information our user needs to provide to create a node or an edge
+ *  2. Is it possible to do OOP for node and edge?
+ *  3. Situations for node: Node created / Node moved / Node deleted
+ *  4. Situations for edge: Edge created / Edge deleted
+ *  
+ */
+
+interface testingCoreStore {
+    nodesStore: Writable<{[key: string]: Node}>
+    edgesStore: Writable<{[key: string]: Edge}>
+    anchorsStore: Writable<{[key: string]: Anchor}>
+}
+
+interface NodeType {
+    id: string,
+    width: number,
+    height: number,
+    positionX: number,
+    positionY: number,
+    bgColor: string,
+    data: string,
+}
+
+interface EdgeType {
+    id: string,
+    type: string,
+    targetId: number,
+    sourceId: number,
+}
+
+interface AnchorType {
+    id: string,
+    nodeId: number,
+    edgeId: number,
+    sourceOrSink: string,
+    getPosition: Function,
+}
+
+export function createStore() {
+    const testingCoreStore: testingCoreStore = {
+        nodesStore: writable({}),
+        edgesStore: writable({}),
+        anchorsStore: writable({}),
+    }
+    return testingCoreStore;
+}
+
+export class Edge implements EdgeType {
+    id: string;
+    sourceId: number;
+    targetId: number;
+    type:string;
+
+    constructor(id:string, sourceId:number, targetId:number, type:string) {
+        this.id = id;
+        //surce is the id of the source node
+        this.sourceId = sourceId;
+        //target is the id of the target node
+        this.targetId = targetId;
+        this.type = type;
+    }
+
+    handleDelete() {
+        console.log('Edge handleDelete fired')
+    }
+} 
+
+//refactore the nodeStore from array to object
+
+export class Anchor implements AnchorType {
+    id: string;
+    nodeId: number;
+    edgeId: number;
+    sourceOrSink: string;
+    getPosition: Function;
+    
+    constructor(id: string, nodeId: number, edgeId: number, sourceOrSink: string, getPosition=() => {
+        //access the nodeStore[node_id]
+        //will look up the node.positionX and node.positionY
+        //will calculate X. Y position of the anchor
+        console.log('Anchor getPosition fired') 
+    }) {
+        this.id = id;
+        this.nodeId = nodeId;
+        this.edgeId = edgeId;
+        this.sourceOrSink = sourceOrSink;
+        this.getPosition = getPosition
+    }
+}
+
+export class Node implements NodeType {
+    id: string
+    width: number
+    height: number
+    positionX: number
+    positionY: number
+    bgColor: string
+    data: string
+
+    constructor(id:string, positionX:number, positionY:number, width:number, height:number, bgColor:string, data:string) {
+        this.id = id;
+        this.positionX = positionX;
+        this.positionY = positionY;
+        this.width = width;
+        this.height = height;
+        this.bgColor = bgColor;
+        this.data = data;
+    }
+
+    setPosition(movementX: number, movementY: number) {
+        //update all necessary data
+        this.positionX += movementX;
+        this.positionY += movementY;
+        console.log('POSX: ', this.positionX)
+        console.log('POSY: ', this.positionY)
+    }
+
+    handleDelete() {
+        console.log('Node handleDelete fired')
+    }
+}
+
