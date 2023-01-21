e1ca550389f79667f5ef9eb23051a678b1aa908b
Michael Chiang <michael.chiang.mc5@gmail.com>
2023-01-21 14:23:44 -0800

    changed DevTools component to Canvas

--- src/routes/testingplayground/index.svelte
+++ src/routes/testingplayground/index.svelte
@@ -1,39 +1,35 @@
 <script>
+  //   import Svelvet from 'svelvet';
+  import Canvas from '$lib/Containers/DevTools/Canvas.svelte';
 
-//   import Svelvet from 'svelvet';
-	import DevTools from '$lib/Containers/DevTools/index.svelte';
-	
+  const initialNodes = [
+    {
+      id: 1, // this is the userLabel
+      type: 'input',
+      position: { x: 450, y: 100 },
+      data: { label: '1' },
+      width: 175,
+      height: 40,
+      bgColor: 'black',
+      sourcePosition: 'bottom',
+    },
+    {
+      id: 2, // this is the userLabel
+      type: 'input',
+      position: { x: 650, y: 100 },
+      data: { label: '2' },
+      width: 175,
+      height: 40,
+      bgColor: 'blue',
+      sourcePosition: 'bottom',
+    },
+  ];
 
-	const initialNodes = [
-		{
-			id: 1, // this is the userLabel
-			type: 'input',
-			position: { x: 450, y: 100 },
-			data: { label: '1' },
-			width: 175,
-			height: 40,
-			bgColor: 'black',
-			sourcePosition: 'bottom'
-		},
-		{
-			id: 2, // this is the userLabel
-			type: 'input',
-			position: { x: 650, y: 100 },
-			data: { label: '2' },
-			width: 175,
-			height: 40,
-			bgColor: 'blue',
-			sourcePosition: 'bottom'
-		},
-	];
-	
-    const initialEdges = [
-		{ id: 'e1-2', source: 1, type: 'straight', target: 2, label: 'e1-2' },
-	];
-
-
+  const initialEdges = [
+    { id: 'e1-2', source: 1, type: 'straight', target: 2, label: 'e1-2' },
+  ];
 </script>
 
 <!-- <Svelvet nodes={initialNodes} edges={initialEdges} width={900} height={900} background /> -->
 
-<DevTools nodes={initialNodes} edges={initialEdges} /> 
+<Canvas nodes={initialNodes} edges={initialEdges} />
