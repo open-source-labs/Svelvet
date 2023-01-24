4a429804ed61577a9664870b49efbfcea2f6e04d
Michael Chiang <michael.chiang.mc5@gmail.com>
2023-01-24 11:23:51 -0800

    removed deprecated files

--- src/lib/views/RefactoredComponents/StepEdge.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-  import type { EdgeType } from '$lib/models/types';
-  import SmoothStepEdge from './SmoothStepEdge.svelte';
-  export let edge: EdgeType;
-</script>
-
-<!-- Utilizes the SmoothStepEdge Component and sets borderRadius to zero to give a sharp edge -->
-<SmoothStepEdge {edge} borderRadius={0} />
