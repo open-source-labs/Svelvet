fdd7e39f1c1f5ae4ba603a3e7816b16c4c13e2b4
Michael Chiang <michael.chiang.mc5@gmail.com>
2023-01-25 07:51:32 -0800

    refactored dynamicCb for anchor into separate file

---
+++ src/lib/controllers/anchorCbDev.ts
@@ -0,0 +1,128 @@
+import { v4 as uuidv4 } from 'uuid';
+import { rightCb, leftCb, topCb, bottomCb } from './anchorCbUser'; // these are callbacks used to calculate anchor position relative to node
+import type {
+  NodeType,
+  EdgeType,
+  AnchorType,
+  StoreType,
+  ResizeNodeType,
+  UserNodeType,
+  UserEdgeType,
+} from '$lib/models/types';
+import { ResizeNode } from '$lib/models/ResizeNode';
+import { Anchor } from '$lib/models/Anchor';
+import { Node } from '$lib/models/Node';
+import { Edge } from '$lib/models/Edge';
+import { writable, derived, get, readable } from 'svelte/store';
+import {
+  getNodes,
+  getAnchors,
+  getNodeById,
+  getAnchorById,
+  getEdgeById,
+} from './storeApi';
+
+export function dynamicCbCreator(
+  store: StoreType,
+  edgeId: string,
+  anchorId: string
+) {
+  return dynamicCb;
+  function dynamicCb() {
+    // get the two anchors
+    const anchors = getAnchors(store, { edgeId: edgeId });
+    if (anchors.length !== 2) throw 'there should be two anchors per edge';
+    let [anchorSelf, anchorOther] = anchors;
+    if (anchorSelf.id !== anchorId)
+      [anchorSelf, anchorOther] = [anchorOther, anchorSelf];
+    // get the two nodes
+    const nodeSelf = getNodeById(store, anchorSelf.nodeId);
+    const nodeOther = getNodeById(store, anchorOther.nodeId);
+    // get the midpoints
+    const [xSelf, ySelf, xOther, yOther] = [
+      nodeSelf.positionX + nodeSelf.width / 2,
+      nodeSelf.positionY + nodeSelf.height / 2,
+      nodeOther.positionX + nodeOther.width / 2,
+      nodeOther.positionY + nodeOther.height / 2,
+    ];
+
+    // record angle for later. We use this so we don't have an infinite loop
+    let prevAngle = anchorSelf.angle;
+
+    // calculate the slope
+    const slope = (ySelf - yOther) / (xSelf - xOther);
+    // slope<1 means -45 to 45 degrees so left/right anchors
+    if (Math.abs(slope) < 1) {
+      // self node is on the left, other node is on the right
+      if (nodeSelf.positionX < nodeOther.positionX) {
+        const [selfX, selfY] = rightCb(
+          nodeSelf.positionX,
+          nodeSelf.positionY,
+          nodeSelf.width,
+          nodeSelf.height
+        );
+        const [otherX, otherY] = leftCb(
+          nodeOther.positionX,
+          nodeOther.positionY,
+          nodeOther.width,
+          nodeOther.height
+        );
+        anchorSelf.setPosition(selfX, selfY);
+        anchorSelf.angle = 0; // if the self node is on the left, the anchor should have orientation of 0 degrees on the unit circle
+      } else {
+        // in this case, the self node is on the right and the other node is on the left
+        const [selfX, selfY] = leftCb(
+          nodeSelf.positionX,
+          nodeSelf.positionY,
+          nodeSelf.width,
+          nodeSelf.height
+        );
+        const [otherX, otherY] = rightCb(
+          nodeOther.positionX,
+          nodeOther.positionY,
+          nodeOther.width,
+          nodeOther.height
+        );
+        anchorSelf.setPosition(selfX, selfY);
+        anchorSelf.angle = 180;
+      }
+    } else {
+      if (nodeSelf.positionY < nodeOther.positionY) {
+        // here the self node is above the other node
+        const [selfX, selfY] = bottomCb(
+          nodeSelf.positionX,
+          nodeSelf.positionY,
+          nodeSelf.width,
+          nodeSelf.height
+        );
+        const [otherX, otherY] = topCb(
+          nodeOther.positionX,
+          nodeOther.positionY,
+          nodeOther.width,
+          nodeOther.height
+        );
+        anchorSelf.setPosition(selfX, selfY);
+        anchorSelf.angle = 270;
+      } else {
+        const [selfX, selfY] = topCb(
+          nodeSelf.positionX,
+          nodeSelf.positionY,
+          nodeSelf.width,
+          nodeSelf.height
+        );
+        const [otherX, otherY] = bottomCb(
+          nodeOther.positionX,
+          nodeOther.positionY,
+          nodeOther.width,
+          nodeOther.height
+        );
+        anchorSelf.setPosition(selfX, selfY);
+        anchorSelf.angle = 90;
+      }
+    }
+
+    // if the anchor changed position, then do operation for other anchor
+    // otherwise, don't do anything. This check is so we don't have an infinite loop
+    if (prevAngle !== anchorSelf.angle) anchorOther.callback();
+  }
+}
