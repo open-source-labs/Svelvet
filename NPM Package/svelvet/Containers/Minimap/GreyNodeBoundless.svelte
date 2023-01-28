be7d97ee8d3835eaeff15e01020b836a332a0154
Ernesto <egonzalez442@gmail.com>
2023-01-27 21:17:56 -0500

    Adds team names to authors section

--- NPM Package/svelvet/Containers/Minimap/GreyNodeBoundless.svelte
+++ NPM Package/svelvet/Containers/Minimap/GreyNodeBoundless.svelte
@@ -1,36 +1,38 @@
 <script>
-    export let key
-    export let node
-    export let heightRatio
-    export let widthRatio
-    export let nodeXleftPosition
-    export let nodeYbottomPosition
-    
-    let top = 0;
-    let left = 0;
-    let nWidth = 0;
-    let nHeight = 0;
-    //nHeight/nWidth repreesnt the height and width of the grey nodes
-    //top/left represent the actual position of the grey nodes on the minimap
-    
-    $: {
-        nHeight = Math.max(node.height * heightRatio, 5)
-        nWidth = Math.max(node.width * widthRatio, 5)
-        top = (node.position.y*heightRatio - nodeYbottomPosition*heightRatio)+1
-        left = (node.position.x* widthRatio - nodeXleftPosition*widthRatio) +1
-    }
+  export let key;
+  export let node;
+  export let heightRatio;
+  export let widthRatio;
+  export let nodeXleftPosition;
+  export let nodeYbottomPosition;
 
+  let top = 0;
+  let left = 0;
+  let nWidth = 0;
+  let nHeight = 0;
+  //nHeight/nWidth represent the height and width of the grey nodes
+  //top/left represent the actual position of the grey nodes on the minimap
+
+  $: {
+    nHeight = Math.max(node.height * heightRatio, 5);
+    nWidth = Math.max(node.width * widthRatio, 5);
+    top = node.position.y * heightRatio - nodeYbottomPosition * heightRatio + 1;
+    left = node.position.x * widthRatio - nodeXleftPosition * widthRatio + 1;
+  }
 </script>
-<div class="nodes nodes-{key}" style="top: {top}px; left: {left}px; height: {nHeight}px; width:{nWidth}px"></div>
-      
+
+<div
+  class="nodes nodes-{key}"
+  style="top: {top}px; left: {left}px; height: {nHeight}px; width:{nWidth}px"
+/>
+
 <style>
-.nodes{
-    position:absolute;
+  .nodes {
+    position: absolute;
     background-color: black;
     color: black;
     opacity: 20%;
     z-index: 9;
-    border-radius: .1rem;
-}
+    border-radius: 0.1rem;
+  }
 </style>
-      
