fd1e350766dc8fc17d4be031aa2f4b04b85a9efc
Michael Chiang <michael.chiang.mc5@gmail.com>
2023-01-27 23:07:57 -0800

    deletes old Svelvet3 code

--- src/libOld/Edges/StepEdge.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-  import SmoothStepEdge from './SmoothStepEdge.svelte';
-  import type { DerivedEdge } from '../types';
-  export let edge: DerivedEdge;
-</script>
-
-<!-- Utilizes the SmoothStepEdge Component and sets borderRadius to zero to give a sharp edge -->
-<SmoothStepEdge {edge} borderRadius={0} />
