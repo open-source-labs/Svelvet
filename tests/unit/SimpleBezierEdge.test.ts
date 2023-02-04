eabd40e2b8806a8cae371750af5c304e79bd67ae
rachelheplus <hejia0917rachel@gmail.com>
2023-02-04 10:09:01 -0500

    Update some tests

--- tests/unit/SimpleBezierEdge.test.ts
+++ tests/unit/SimpleBezierEdge.test.ts
@@ -1,82 +1,97 @@
 import SimpleBezierEdge from '$lib/edges/views/Edges/SimpleBezierEdge.svelte';
+
 import { render, screen, cleanup } from '@testing-library/svelte';
+
+import { v4 as uuidv4 } from 'uuid';
+import {
+  createStoreEmpty,
+  populateSvelvetStoreFromUserInput,
+} from '$lib/store/controllers/storeApi';
+import { sanitizeUserNodesAndEdges } from '$lib/container/controllers/middleware';
+import type { UserNodeType, UserEdgeType } from '$lib/store/types/types';
 
 const bottom_top = {
   id: 'e1-2',
+  source: '1',
+  target: '2',
   sourceX: 100,
   sourceY: 100,
   targetX: 200,
   targetY: 200,
   sourcePosition: 'bottom',
   targetPosition: 'top',
-  data: {
-    label: 'this is the test edge',
-  },
+  label: 'this is the test edge',
 };
+
 const right_left = {
   id: 'e1-2',
+  source: '1',
+  target: '2',
   sourceX: 100,
   sourceY: 100,
   targetX: 200,
   targetY: 200,
   sourcePosition: 'right',
   targetPosition: 'left',
-  data: {
-    label: 'this is the test edge',
-  },
+  label: 'this is the test edge',
 };
 
 const left_right = {
   id: 'e1-2',
+  source: '1',
+  target: '2',
   sourceX: 300,
   sourceY: 100,
   targetX: 200,
   targetY: 200,
   sourcePosition: 'left',
   targetPosition: 'right',
-  data: {
-    label: 'this is the test edge',
-  },
+  label: 'this is the test edge',
 };
 
 const top_left = {
   id: 'e1-2',
+  source: '1',
+  target: '2',
   sourceX: 100,
   sourceY: 300,
   targetX: 200,
   targetY: 200,
   sourcePosition: 'top',
   targetPosition: 'left',
-  data: {
-    label: 'this is the test edge',
-  },
+  label: 'this is the test edge',
 };
 
 const top_right = {
   id: 'e1-2',
+  source: '1',
+  target: '2',
   sourceX: 100,
   sourceY: 400,
   targetX: 200,
   targetY: 200,
   sourcePosition: 'top',
   targetPosition: 'right',
-  data: {
-    label: 'this is the test edge',
-  },
+  label: 'this is the test edge',
 };
 
 const bottom_left = {
   id: 'e1-2',
+  source: '1',
+  target: '2',
   sourceX: 100,
   sourceY: 100,
   targetX: 200,
   targetY: 200,
   sourcePosition: 'bottom',
   targetPosition: 'left',
-  data: {
-    label: 'this is the test edge',
-  },
+  label: 'this is the test edge',
 };
+
+
+
+const canvasId = uuidv4();
+const initialEdges: UserEdgeType[] = [bottom_top, right_left, left_right, top_left, top_right, bottom_left]
 
 test('should calculate the correct bezier path string from bottom to top', () => {
   render(SimpleBezierEdge, { props: { edge: bottom_top } });
