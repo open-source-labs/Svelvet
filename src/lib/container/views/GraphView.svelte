45133f29c21a60ad25264594462cc5ae0ecaf497
Michael Chiang <michael.chiang.mc5@gmail.com>
2023-02-04 00:26:20 -0800

    minimap zoom seems to be wokring

--- src/lib/container/views/GraphView.svelte
+++ src/lib/container/views/GraphView.svelte
@@ -77,7 +77,8 @@
     gridSize,
     dotSize,
     canvasId,
-    d3Scale
+    d3Scale,
+    handleZoom
   );
 
   // d3Translate is used for the minimap
@@ -132,11 +133,43 @@
   }
 
   const minimap = true; // QQQ
-  // boundary = { x: 1000, y: 1000 };
-  const miniMapClick2 = () => {
-    console.log('minimapClick');
-  };
   const key = canvasId;
+
+  function handleZoom(e) {
+    if (!$movementStore) return;
+    //add a store that contains the current value of the d3-zoom's scale to be used in onMouseMove function
+    d3Scale.set(e.transform.k);
+    // should not run d3.select below if backgroundStore is false
+    if (backgroundStore) {
+      d3.select(`#background-${canvasId}`)
+        .attr('x', e.transform.x)
+        .attr('y', e.transform.y)
+        .attr('width', gridSize * e.transform.k)
+        .attr('height', gridSize * e.transform.k)
+        .selectAll('#dot')
+        .attr('x', (gridSize * e.transform.k) / 2 - dotSize / 2)
+        .attr('y', (gridSize * e.transform.k) / 2 - dotSize / 2)
+        .attr('opacity', Math.min(e.transform.k, 1));
+    }
+    // transform 'g' SVG elements (edge, edge text, edge anchor)
+    d3.select(`.Edges-${canvasId} g`).attr('transform', e.transform);
+    // transform div elements (nodes)
+    let transform = d3.zoomTransform(this);
+    d3Translate = transform;
+    // selects and transforms all node divs from class 'Node' and performs transformation
+    d3.select(`.Node-${canvasId}`)
+      .style(
+        'transform',
+        'translate(' +
+          transform.x +
+          'px,' +
+          transform.y +
+          'px) scale(' +
+          transform.k +
+          ')'
+      )
+      .style('transform-origin', '0 0');
+  }
 </script>
 
 <!-- This is the container that holds GraphView and we have disabled right click functionality to prevent a sticking behavior -->
--- src/lib/d3/controllers/d3.ts
+++ src/lib/d3/controllers/d3.ts
@@ -66,7 +66,8 @@ export function determineD3Instance(
   gridSize,
   dotSize,
   canvasId,
-  d3Scale
+  d3Scale,
+  handleZoom
 ) {
   if (boundary) {
     return d3
@@ -88,42 +89,5 @@ export function determineD3Instance(
       .filter(() => !get(nodeSelected))
       .scaleExtent([0.4, 2])
       .on('zoom', handleZoom);
-  }
-
-  function handleZoom(e) {
-    if (!get(movementStore)) return;
-    //add a store that contains the current value of the d3-zoom's scale to be used in onMouseMove function
-    d3Scale.set(e.transform.k);
-    // should not run d3.select below if backgroundStore is false
-    if (backgroundStore) {
-      d3.select(`#background-${canvasId}`)
-        .attr('x', e.transform.x)
-        .attr('y', e.transform.y)
-        .attr('width', gridSize * e.transform.k)
-        .attr('height', gridSize * e.transform.k)
-        .selectAll('#dot')
-        .attr('x', (gridSize * e.transform.k) / 2 - dotSize / 2)
-        .attr('y', (gridSize * e.transform.k) / 2 - dotSize / 2)
-        .attr('opacity', Math.min(e.transform.k, 1));
-    }
-    // transform 'g' SVG elements (edge, edge text, edge anchor)
-    d3.select(`.Edges-${canvasId} g`).attr('transform', e.transform);
-    // transform div elements (nodes)
-    let transform = d3.zoomTransform(this);
-    let d3Translate = transform;
-    console.log('1111');
-    // selects and transforms all node divs from class 'Node' and performs transformation
-    d3.select(`.Node-${canvasId}`)
-      .style(
-        'transform',
-        'translate(' +
-          transform.x +
-          'px,' +
-          transform.y +
-          'px) scale(' +
-          transform.k +
-          ')'
-      )
-      .style('transform-origin', '0 0');
   }
 }
