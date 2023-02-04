4463b783bc482f0adb86980831493bf273d864a2
rachelheplus <hejia0917rachel@gmail.com>
2023-02-04 16:55:01 -0500

    Create feature pages for Resizable Nodes and Dynamic Anchors

---
+++ src/routes/docs/ResizableNodes.svelte
@@ -0,0 +1,32 @@
+<script lang='ts'>
+
+</script>
+
+<div>
+<h2 class="text-4xl font-semibold mb-12">Resizable Nodes</h2>
+</div>
+
+<p class="text-gray-600">
+  <code class="code">Svelvet</code> now makes nodes more powerful by enabling <code class="code">resizability</code> as a new default feature for all the nodes in Svelvet component. Hover your mouse to the right bottom corner of each node, you can see a purple mini dot attached. Drag the purple dot and see what happens!
+</p>
+
+
+<h3 class="text-xl font-semibold mt-12 mb-6">Example of Resizable Nodes</h3>
+<p class="text-gray-600">
+  See the Code Sandbox below for the implementation!
+</p>
+
+<br />
+
+
+
+<iframe src="https://codesandbox.io/embed/feature-resizable-nodes-unnyfj?fontsize=14&hidenavigation=1&theme=dark"
+  style="width:100%; height:500px; border:0; border-radius: 4px; overflow:hidden;"
+  title="Feature Resizable Nodes"
+  allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
+  sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
+></iframe>
+
+<style>
+
+</style>
