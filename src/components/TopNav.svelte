5a01a86fb0017276e01f50a243ec28acc7ab7f83
Michael Chiang <michael.chiang.mc5@gmail.com>
2023-02-07 16:48:02 -0800

    changed 5.0.7 to 6.0.0 in navbar prettified topNavbar

--- src/components/TopNav.svelte
+++ src/components/TopNav.svelte
@@ -37,21 +37,31 @@
       on:click={() => {
         activeLink = '/';
       }}
-      class="text-3xl text-gray-700 font-nunito font-medium tracking-wide ml-2 mr-6">svelvet</a
+      class="text-3xl text-gray-700 font-nunito font-medium tracking-wide ml-2 mr-6"
+      >svelvet</a
     >
 
-    <p class="text-xs rounded-full px-4 py-1 bg-rose-100 text-red-400 tracking-wider">v5.0.7</p>
+    <p
+      class="text-xs rounded-full px-4 py-1 bg-rose-100 text-red-400 tracking-wider"
+    >
+      v6.0.0
+    </p>
   </div>
 
   <nav class="space-x-11 text-sm text-gray-500 font-medium flex items-center">
-    <a href="/" id="home" class="hover:text-rose-500 {activeLink === '/' ? 'text-rose-500' : ''}"
+    <a
+      href="/"
+      id="home"
+      class="hover:text-rose-500 {activeLink === '/' ? 'text-rose-500' : ''}"
       >Home</a
     >
 
     <a
       href="/docs/installation"
       id="docs"
-      class="hover:text-rose-500 {activeLink.includes('docs') ? 'text-rose-500' : ''}">Docs</a
+      class="hover:text-rose-500 {activeLink.includes('docs')
+        ? 'text-rose-500'
+        : ''}">Docs</a
     >
 
     <!-- <a
@@ -59,8 +69,6 @@
     id="community"
     class="hover:text-rose-500 {activeLink.includes('community') ? 'text-rose-500' : ''}">Community</a
     > -->
- 
-
 
     <a
       href="https://github.com/open-source-labs/Svelvet"
@@ -72,22 +80,51 @@
     <a
       href="/playground"
       id="playground"
-      class="hover:text-rose-500 {activeLink.includes('REPL') ? 'text-rose-500' : ''}">REPL</a
+      class="hover:text-rose-500 {activeLink.includes('REPL')
+        ? 'text-rose-500'
+        : ''}">REPL</a
     >
 
-    
-      <div class="dropdown">
-        <button class="dropbtn hover:text-rose-500">Blogs
-          <i class="fa fa-caret-down"></i>
-        </button>
-        <div class="dropdown-content">
-          <a target="_blank" class="hover:text-rose-500 {activeLink.includes('blog') ? 'text-rose-500' : ''}" href="https://medium.com/@alexander.zambrano/simplify-application-diagramming-with-svelvet-a8f664731243">Svelvet 1.0</a>
-          <a target="_blank" class="hover:text-rose-500 {activeLink.includes('blog') ? 'text-rose-500' : ''}" href="https://medium.com/gitconnected/svelvet-2-0-c6b2059734a6">Svelvet 2.0</a>
-          <a target="_blank" class="hover:text-rose-500 {activeLink.includes('blog') ? 'text-rose-500' : ''}" href="https://medium.com/@MauricioACastro/svelvet-4-0-the-power-of-html-is-now-inside-your-nodes-3d96823096e3">Svelvet 4.0</a>
-          <a target="_blank" class="hover:text-rose-500 {activeLink.includes('blog') ? 'text-rose-500' : ''}" href="https://medium.com/@efergus1/svelvet-5-0-a-community-driven-update-cfcc93e7b7a7">Svelvet 5.0</a>
-        </div>
+    <div class="dropdown">
+      <button class="dropbtn hover:text-rose-500"
+        >Blogs
+        <i class="fa fa-caret-down" />
+      </button>
+      <div class="dropdown-content">
+        <a
+          target="_blank"
+          class="hover:text-rose-500 {activeLink.includes('blog')
+            ? 'text-rose-500'
+            : ''}"
+          href="https://medium.com/@alexander.zambrano/simplify-application-diagramming-with-svelvet-a8f664731243"
+          >Svelvet 1.0</a
+        >
+        <a
+          target="_blank"
+          class="hover:text-rose-500 {activeLink.includes('blog')
+            ? 'text-rose-500'
+            : ''}"
+          href="https://medium.com/gitconnected/svelvet-2-0-c6b2059734a6"
+          >Svelvet 2.0</a
+        >
+        <a
+          target="_blank"
+          class="hover:text-rose-500 {activeLink.includes('blog')
+            ? 'text-rose-500'
+            : ''}"
+          href="https://medium.com/@MauricioACastro/svelvet-4-0-the-power-of-html-is-now-inside-your-nodes-3d96823096e3"
+          >Svelvet 4.0</a
+        >
+        <a
+          target="_blank"
+          class="hover:text-rose-500 {activeLink.includes('blog')
+            ? 'text-rose-500'
+            : ''}"
+          href="https://medium.com/@efergus1/svelvet-5-0-a-community-driven-update-cfcc93e7b7a7"
+          >Svelvet 5.0</a
+        >
       </div>
-  
+    </div>
 
     <!-- Add logic for OAuth and conditionally render if the user is logged in, change button text to sign out and vice versa -->
 
@@ -137,58 +174,57 @@
     border-radius: 50%;
   }
 
-   /* Navbar container */
+  /* Navbar container */
 
+  /* The dropdown container */
+  .dropdown {
+    float: left;
+    overflow: hidden;
+  }
 
+  /* Dropdown button */
+  .dropdown .dropbtn {
+    font-size: 0.875rem;
+    line-height: 1.25rem;
+    text-decoration: inherit;
+    font-weight: 500; /* Important for vertical align on mobile phones */
+    margin: 0; /* Important for vertical align on mobile phones */
+  }
 
-/* The dropdown container */
-.dropdown {
-  float: left;
-  overflow: hidden;
-}
+  /* Add a red background color to navbar links on hover */
+  .dropdown a:hover,
+  .dropdown:hover .dropbtn {
+    background-color: rgb(255, 255, 255);
+    color: #ff4561;
+  }
 
-/* Dropdown button */
-.dropdown .dropbtn {
-  font-size: 0.875rem;
-line-height: 1.25rem;
-text-decoration: inherit;
-  font-weight: 500;/* Important for vertical align on mobile phones */
-  margin: 0; /* Important for vertical align on mobile phones */
-}
+  /* Dropdown content (hidden by default) */
+  .dropdown-content {
+    display: none;
+    position: absolute;
+    background-color: #f9f9f9;
+    min-width: 160px;
+    box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+    z-index: 1;
+  }
 
-/* Add a red background color to navbar links on hover */
-.dropdown a:hover, .dropdown:hover .dropbtn {
-  background-color: rgb(255, 255, 255);
-  color: #ff4561;
-}
+  /* Links inside the dropdown */
+  .dropdown-content a {
+    float: none;
+    color: black;
+    padding: 12px 16px;
+    text-decoration: none;
+    display: block;
+    text-align: left;
+  }
 
-/* Dropdown content (hidden by default) */
-.dropdown-content {
-  display: none;
-  position: absolute;
-  background-color: #f9f9f9;
-  min-width: 160px;
-  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-  z-index: 1;
-}
+  /* Add a grey background color to dropdown links on hover */
+  .dropdown-content a:hover {
+    background-color: #ddd;
+  }
 
-/* Links inside the dropdown */
-.dropdown-content a {
-  float: none;
-  color: black;
-  padding: 12px 16px;
-  text-decoration: none;
-  display: block;
-  text-align: left;
-}
-
-/* Add a grey background color to dropdown links on hover */
-.dropdown-content a:hover {
-  background-color: #ddd;
-}
-
-/* Show the dropdown menu on hover */
-.dropdown:hover .dropdown-content {
-  display: block;
-} 
+  /* Show the dropdown menu on hover */
+  .dropdown:hover .dropdown-content {
+    display: block;
+  }
 </style>
