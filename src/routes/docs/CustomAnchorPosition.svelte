f7cdd035e58bdc380e3fa72c9dfd93d5cab38909
rachelheplus <hejia0917rachel@gmail.com>
2023-02-08 17:38:04 -0500

    Add gif and StackBlitz to CustomAnchorPosition feature page

--- src/routes/docs/CustomAnchorPosition.svelte
+++ src/routes/docs/CustomAnchorPosition.svelte
@@ -1,5 +1,5 @@
 <script>
-
+  import customAnchor from '../../assets/features/feature_custom_anchors.gif';
 </script>
 
 <div>
@@ -7,20 +7,17 @@
 </div>
 
 <p class="text-gray-600">
-  The default anchors can appear at the top, bottom, left, or right center of the node but it does not always satisfy developers' specific needs. So Svelvet 6.0 introduces <code class="code">custom anchor position</code> as a new feature. Developers can now pass in a <code class="code">customed callback function</code> to decide where they want the anchors to appear on the node. 
+  The default anchors can appear at the top, bottom, left, or right center of the node but it does not always satisfy developers' specific needs. So Svelvet 6.0 introduces <code class="code">custom anchor position</code> as a new feature. Developers can now pass in a <code class="code">custom callback function</code> to decide where they want the anchors to appear on the node. 
 </p>
 
 <br/>
-<p class="text-gray-600">TODO: Add intruction of how to create a custom anchor position callback function</p>
+<img alt="Custom Anchor position demo" src={customAnchor} />
 
 
 <h3 class="text-xl font-semibold mt-12 mb-6">Custom Anchor Position</h3>
 <p class="text-gray-600">
   See the Code Sandbox below for the implementation!
 </p>
-
-<br />
-
 <iframe src="https://codesandbox.io/embed/jolly-tess-p8vxf9?fontsize=14&hidenavigation=1&theme=dark"
   style="width:100%; height:500px; border:0; border-radius: 4px; overflow:hidden;"
   title="jolly-tess-p8vxf9"
@@ -28,4 +25,14 @@
   sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
 ></iframe>
 
+<br />
 
+<p class="text-gray-600">
+  See the Stackblitz below for the implementation!
+</p>
+<iframe src="https://stackblitz.com/edit/sveltejs-kit-template-default-w7fvtb?embed=1&file=src/routes/+page.svelte"
+  style="width:100%; height:500px; border:0; border-radius: 4px; overflow:hidden;"
+  title="jolly-tess-p8vxf9"
+  allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
+  sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
+></iframe> 
