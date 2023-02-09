f2ad3cab86c156dcd9d72cc3abffe91523c514de
Yonathan Abramov <70810810+ya-johnson@users.noreply.github.com>
2023-02-09 15:53:54 +0200

    update tailwind config - add primray color and configure container

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,10 @@
 module.exports = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
+		container: {
+			center: true,
+			padding: '1rem'
+		},
 		fontFamily: {
 			inter: ['Inter', 'sans-serif'],
 			nunito: ['Nunito', 'sans-serif']
@@ -8,6 +12,9 @@ module.exports = {
 		extend: {
 			screens: {
 				contributors: { raw: '(min-width: 935px)' }
+			},
+			colors: {
+				primary: '#ff4561',
 			}
 		}
 	},
