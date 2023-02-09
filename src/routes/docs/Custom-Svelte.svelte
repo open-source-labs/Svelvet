2c321c88b7387799ff332e147557326919197780
Michael Chiang <michael.chiang.mc5@gmail.com>
2023-02-09 12:23:27 -0800

    FIX: made Custom-Svelte-Component user documentation look nicer

--- src/routes/docs/Custom-Svelte.svelte
+++ src/routes/docs/Custom-Svelte.svelte
@@ -1,4 +1,39 @@
 <script>
+  import Svelvet from 'svelvet';
+  import Login from './assets/Login.svelte';
+  import Home from './assets/Home.svelte';
+  import About from './assets/About.svelte';
+
+  const initialNodes = [
+    {
+      id: 1,
+      position: { x: -550, y: -200 },
+      data: { custom: Login },
+      width: 300 * 0.8,
+      height: 300 * 0.8,
+      bgColor: 'white',
+    },
+    {
+      id: 2,
+      position: { x: -850, y: 250 },
+      data: { custom: Home },
+      width: 300,
+      height: 300,
+      bgColor: 'white',
+    },
+    {
+      id: 3,
+      position: { x: -450, y: 250 },
+      data: { custom: About },
+      width: 300,
+      height: 300,
+      bgColor: 'white',
+    },
+  ];
+
+  const initialEdges = [
+    { id: 'e1-2', source: 1, target: 2, label: 'connection' },
+  ];
 </script>
 
 <h2 class="text-4xl font-semibold mb-12">Custom Svelte Components</h2>
@@ -14,12 +49,6 @@
   Svelte component.
 </p>
 
-<div>
-  <h3 class="text-xl font-semibold mt-12 mb-6">
-    Implementing Custom Svelte Components
-  </h3>
-  <p class="text-gray-600">See the Code Sandbox below to see implementation!</p>
-</div>
 <!-- 
 <iframe src="https://codesandbox.io/embed/custom-svelte-components-p4ztve?autoresize=1&fontsize=14&hidenavigation=1&module=%2FApp.svelte&theme=dark"
      style="width:100%; height:500px; border:0; border-radius: 4px; overflow:hidden;"
@@ -29,18 +58,28 @@
 ></iframe>
  -->
 
-<iframe
-  src="https://stackblitz.com/edit/custom-svelte?embed=1&file=src/routes/About.svelte"
-  style="width:100%; height:500px; border:0; border-radius: 4px; overflow:hidden;"
-  title="adaptive-anchors-demo"
-  allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
-  sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
-/>
+<div>
+  <h3 class="text-xl font-semibold mt-12 mb-6">
+    Implementing Custom Svelte Components
+  </h3>
+  <p class="text-gray-600">
+    Click <a
+      href="https://stackblitz.com/edit/custom-svelte?file=src/routes/About.svelte"
+      style="color: blue">here</a
+    > for a sandbox, or try it yourself below!
+  </p>
+</div>
+<br />
 
-<p>
-  StackBlitz embeds do not work on non-Chromium browsers. If you are using
-  Firefox, use this direct <a
-    href="https://stackblitz.com/edit/custom-svelte?file=src/routes/About.svelte"
-    style="color: blue">link</a
-  > instead
-</p>
+<Svelvet
+  nodes={initialNodes}
+  width={500}
+  height={500}
+  initialZoom={0}
+  initialLocation={initialNodes[2].position}
+  edges={initialEdges}
+  bgColor={'#EEEEEE'}
+  nodeLink
+  nodeCreate
+  background
+/>
---
+++ src/routes/docs/assets/Login.svelte
@@ -0,0 +1,78 @@
+<script>
+  // Import the required components
+
+  // Define the variables for the username and password
+  let username = '';
+  let password = '';
+
+  let user = { loggedIn: false };
+  function toggle() {
+    user.loggedIn = !user.loggedIn;
+  }
+</script>
+
+{#if !user.loggedIn}
+  <h1>svelvet demo</h1>
+  <div class="form">
+    <input type="text" placeholder="Username" />
+    <input type="password" placeholder="Password" />
+    <button class="button" on:click={toggle}> Login Here </button>
+  </div>
+{:else}
+  <div class="logout">
+    <h1>You are logged in</h1>
+    <button class="button" on:click={toggle}> Logout </button>
+  </div>
+{/if}
+
+<style>
+  /* Add some basic styles to the login form */
+  h1 {
+    color: #ff4742;
+  }
+  .form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+  .logout {
+    display: flex;
+    flex-direction: column;
+  }
+  .button {
+    background: #ff4742;
+    border: 1px solid #ff4742;
+    border-radius: 6px;
+    box-shadow: rgba(0, 0, 0, 0.1) 1px 2px 4px;
+    box-sizing: border-box;
+    color: #ffffff;
+    cursor: pointer;
+    display: inline-block;
+    font-family: nunito, roboto, proxima-nova, 'proxima nova', sans-serif;
+    font-size: 16px;
+    font-weight: 800;
+    line-height: 16px;
+    min-height: 40px;
+    outline: 0;
+    padding: 12px 14px;
+    text-align: center;
+    text-rendering: geometricprecision;
+    text-transform: none;
+    user-select: none;
+    -webkit-user-select: none;
+    touch-action: manipulation;
+    vertical-align: middle;
+  }
+
+  .button:hover,
+  .button:active {
+    background-color: initial;
+    background-position: 0 0;
+    color: #ff4742;
+  }
+
+  .button:active {
+    opacity: 0.5;
+  }
+</style>
