d2f057260826faa0da57c711d30e59158e33b38e
briangregoryholmes <briangregoryholmes@gmail.com>
2023-04-25 13:33:25 -0400

    test: added test for connection prop on Node and Anchor

---
+++ tests/connections/test.ts
@@ -0,0 +1,28 @@
+import { expect, test } from '@playwright/test';
+
+const testRoute = '/connections';
+
+test('connections are made correctly', async ({ page }) => {
+	await page.goto(testRoute);
+
+	await expect(await page.locator('.edge')).toHaveCount(15);
+
+	await expect(await page.locator('[id="A-1/N-1+A-1/N-2"]')).toBeVisible();
+	await expect(await page.locator('[id="A-1/N-3+A-2/N-1"]')).toBeVisible();
+	await expect(await page.locator('[id="A-1/N-4+A-3/N-1"]')).toBeVisible();
+	await expect(await page.locator('[id="A-1/N-5+A-4/N-1"]')).toBeVisible();
+	await expect(await page.locator('[id="A-1/N-1+A-1/N-6"]')).toBeVisible();
+	await expect(await page.locator('[id="A-2/N-1+A-custom/N-custom"]')).toBeVisible();
+
+	await expect(await page.locator('[id="A-1/N-2+A-2/N-8"]')).toBeVisible();
+	await expect(await page.locator('[id="A-2/N-3+A-2/N-8"]')).toBeVisible();
+	await expect(await page.locator('[id="A-2/N-4+A-2/N-8"]')).toBeVisible();
+	await expect(await page.locator('[id="A-1/N-5+A-2/N-8"]')).toBeVisible();
+	await expect(await page.locator('[id="A-1/N-6+A-2/N-8"]')).toBeVisible();
+	await expect(await page.locator('[id="A-2/N-8+A-custom/N-custom"]')).toBeVisible();
+
+	await expect(await page.locator('[id="A-5/N-5+A-custom/N-custom"]')).toBeVisible();
+	await expect(await page.locator('[id="A-2/N-5+A-custom/N-custom"]')).toBeVisible();
+
+	await expect(await page.locator('[id="A-1/N-8+A-6/N-5"]')).toBeVisible();
+});
