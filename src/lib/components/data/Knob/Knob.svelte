521b32e71d13ad19f67870b9fb76879991d84786
sam <sam@cps-macbook-pro.lan>
2023-05-13 16:42:41 -0700

    adding knobComponent as a branch to github

---
+++ src/lib/components/data/Knob/Knob.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import type { Graph, Node, CustomWritable } from '$lib/types';
+	// Props
+	export let parameterStore: CustomWritable<number>;
+	export let min: number = 0;
+	export let max: number = 100;
+	export let step: number = 1;
+	export let label: string = 'Value';
+	export let fixed: number = 2;
+</script>
---
+++ src/routes/knob-test/+page.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import { Svelvet, Node } from '$lib';
+	import InputNode from '../../example-components/InputNode.svelte';
+	import { generateInput, generateOutput, Slider } from '$lib';
+	import NodeWrapper from '../../example-components/test-components/NodeWrapper.svelte';
+	type Inputs = {
+		length: number;
+	};
+
+	const initialData = {
+		length: 10
+	};
+	const inputs = generateInput(initialData);
+	const procesor = (inputs: Inputs) => inputs.length;
+	const output = generateOutput(inputs, procesor);
+</script>
+
+<body>
+	<Svelvet>
+		<Node useDefaults position={{ x: 40, y: 158 }} let:selected>
+			<NodeWrapper title="Dash Count" outputStore={output} key="dashCount">
+				<div class="node-body">
+					<!-- REPLACE SLIDER COMPONENT WITH KNOB COMPONENT -->
+					<Slider min={-100} fixed={0} max={100} step={5} parameterStore={$inputs.length} />
+				</div>
+			</NodeWrapper>
+		</Node>
+	</Svelvet>
+</body>
+
+<style>
+	body {
+		width: 100vw;
+		height: 100vh;
+		margin: 0;
+		padding: 0;
+	}
+</style>
