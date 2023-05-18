4a3b83043827269aa1297e9b87dee47ea6a111e4
Welsey-Waters <wesleyswaters@yahoo.com>
2023-05-17 20:36:49 -0700

    add route for testing drawer component

---
+++ src/routes/drawer-test/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { Svelvet, Node} from '$lib';
+	import InputNode from '../../example-components/InputNode.svelte';
+	import {  Drawer } from '$lib';	
+</script>
+
+<body>
+	<Svelvet drawer>
+		<Node useDefaults>
+        
+		</Node>
+	</Svelvet>
+</body>
+
+<style>
+	body {
+		width: 100vw;
+		height: 100vh;
+		margin: 0;
+		padding: 0;
+	}
+</style>
