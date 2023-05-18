5d8663f54e7e443dc2961c68744f88a73d19a36d
Welsey-Waters <wesleyswaters@yahoo.com>
2023-05-17 20:33:55 -0700

    add drawer variable and functionality for adding a drawer component to the graph when a drawer component is created

--- src/lib/containers/Graph/Graph.svelte
+++ src/lib/containers/Graph/Graph.svelte
@@ -42,6 +42,7 @@
 	export let modifier: 'alt' | 'ctrl' | 'shift' | 'meta';
 	export let theme = 'light';
 	export let title: string;
+	export let drawer: boolean = false;
 
 	// Local constants
 	const dispatch = createEventDispatcher();
@@ -80,6 +81,7 @@
 	let toggleComponent: ComponentType | null = null;
 	let minimapComponent: ComponentType | null = null;
 	let controlsComponent: ComponentType | null = null;
+	let drawerComponent: ComponentType | null = null;
 
 	// Subscriptions
 	$: dimensions = $dimensionsStore;
@@ -97,6 +99,8 @@
 	$: if (minimap && !minimapComponent) loadMinimap();
 
 	$: if (controls && !controlsComponent) loadControls();
+
+	$: if (drawer && !drawerComponent) loadDrawer();
 
 	// This is a temporary workaround for generating an edge where one of the anchors is the cursor
 	const cursorAnchor: CursorAnchor = {
@@ -160,6 +164,10 @@
 
 	async function loadControls() {
 		controlsComponent = (await import('$lib/components/Controls/Controls.svelte')).default;
+	}
+
+	async function loadDrawer() {
+		drawerComponent = (await import ('$lib/components/Drawer/Drawer.svelte')).default;
 	}
 
 	function updateGraphDimensions() {
@@ -506,8 +514,11 @@
 	{#if toggle}
 		<svelte:component this={toggleComponent} />
 	{/if}
+	{#if drawer}
+		<svelte:component this={drawerComponent}/>
+	{/if}
 	<slot name="minimap" />
-
+	<slot name='drawer'/>
 	<slot name="controls" />
 	<slot name="toggle" />
 	{#if selecting && !disableSelection}
