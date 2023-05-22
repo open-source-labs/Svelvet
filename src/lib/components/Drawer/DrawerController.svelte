50ec6189bae5d3ca658b2a09f4778bdd12eb1da5
Welsey-Waters <wesleyswaters@yahoo.com>
2023-05-21 22:33:16 -0700

    add local variables, drag and drop functions, button click functionality, and inputs for retrieving data from user

--- src/lib/components/Drawer/DrawerController.svelte
+++ src/lib/components/Drawer/DrawerController.svelte
@@ -1,23 +1,33 @@
 <script lang="ts">
-  import { getContext } from 'svelte';
-	import type { CSSColorString, Graph, Corner } from '$lib/types';
-  import type { Node } from '$lib/types';
-	import { calculateRelativeCursor } from '$lib/utils';
-  
+ 	import type { NodeConfig, CSSColorString, Direction} from '$lib/types';
+	import DrawerNode from './DrawerNode.svelte';
+	import DrawerAnchor from './DrawerAnchor.svelte';
+	import DrawerEdge from './DrawerEdge.svelte';
 
-  let graph: Graph = getContext<Graph>('graph');
+	let defaultNodes: NodeConfig[] = [];
+  let customNodes: NodeConfig[] = [];
+  let anchors: any[] = [];
+  let edges: any[] = [];
+  let customEdge: any;
+  let dropped_in :boolean;
+	
+	const handleDragStart = (e: any)  => {
+    e.dataTransfer.dropEffect = "move"; 
+  }
 
+	const handleDragEnd = (e: any) => {
+    dropped_in = false;
+  }
   
 
 </script>
 
-
-<nav class ='drawerWrapper'>
-    <button>Node<button>    
-</nav>
-
-
-
+	<nav class ='drawerWrapper'>
+			<div class='defaultNodes' draggable='true' on:dragstart={handleDragStart} on:dragend={handleDragEnd}> Node </div>
+			<DrawerNode></DrawerNode>
+			<DrawerAnchor></DrawerAnchor>
+			<DrawerEdge></DrawerEdge>
+	</nav>
 
 
 <style>
@@ -38,81 +48,3 @@
 
 </style>
 
-
-
-<!-- <script lang="ts">
-	import { Node, Svelvet } from '$lib';
-	import type { NodeConfig } from '$lib/types';
-	let dropZoneClass = 'inactive';
-	let NODE_DATA_TYPE = 'node';
-	let nodes: NodeConfig[] = [];
-	const onDragStart = (e: any) => {
-		e.dataTransfer.setData(NODE_DATA_TYPE, e.target.dataset.value);
-	};
-
-	const onDragOver = (e: Event) => {
-		e.preventDefault();
-		return false;
-	};
-
-	const onDragEnter = (e: Event) => {
-		dropZoneClass = 'active';
-	};
-
-	const onDragLeave = (e: Event) => {
-		dropZoneClass = 'inactive';
-	};
-
-	const onDrop = (e: any) => {
-		e.stopPropagation();
-		//Issue click event
-		const moveEvent = new MouseEvent('mousemove', {
-			clientX: e.clientX,
-			clientY: e.clientY,
-			bubbles: true
-		});
-		e.target.dispatchEvent(moveEvent);
-
-		dropZoneClass = 'inactive';
-		const nodeType: string = e.dataTransfer.getData(NODE_DATA_TYPE);
-		nodes = [...nodes, { id: nodes.length }];
-		return false;
-	};
-</script>
-
-
-	<div
-		class={dropZoneClass}
-		on:dragover={onDragOver}
-		on:dragenter={onDragEnter}
-		on:dragleave={onDragLeave}
-		on:drop={onDrop}
-	>
-	    <Svelvet height={800}  zoom={0.75} minimap controls></Svelvet>
-	
-			{#each nodes as node (node.id)}
-				<Node {...node} drop="cursor" />
-			{/each}
-		</Svelvet>	
-</div>
-	
-	
-
-
-<div class="toolbox">
-	<h3 class="title">Toolbox</h3>
-	<ul class="toolbox-items">
-		<li class="list-item" draggable="true" data-value="green" on:dragstart={onDragStart}>
-			<div class="icon green" />
-			Green Square
-		</li>
-		<li class="list-item" draggable="true" data-value="blue" on:dragstart={onDragStart}>
-			<div class="icon blue" />
-			Blue Square
-		</li>
-		<li class="list-item" draggable="true" data-value="red" on:dragstart={onDragStart}>
-			<div class="icon red" />
-			Red Square
-		</li>
-	</ul>
-</div> -->
