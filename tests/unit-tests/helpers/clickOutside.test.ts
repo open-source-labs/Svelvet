a3b7f5c8a287df3b0169c3304910c90e7f3ff004
Ruxin Zheng <RuxinZ@users.noreply.github.com>
2023-05-27 14:52:44 -0700

    test(utils/helpers): set up test for clickOuside, add comments to clickOuside

---
+++ tests/unit-tests/helpers/clickOutside.test.ts
@@ -0,0 +1,14 @@
+import { describe, expect, it, beforeEach, afterEach, vi } from 'vitest';
+import { clickOutside } from '../../../src/lib/utils/helpers/clickOutside';
+import { render } from '@testing-library/svelte';
+
+describe('clickOutside', () => {
+	afterEach(() => {
+		vi.restoreAllMocks();
+	});
+
+	it('should dispatch a CustomEvent called "outclick" to node', () => {
+		expect(1 + 1).toEqual(2);
+		// expect().toHaveBeenCalledTimes(1)
+	});
+});
