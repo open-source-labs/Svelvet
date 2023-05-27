806ed90d3f55c20a6f1ccf2189e2890938e0ebda
Ruxin Zheng <RuxinZ@users.noreply.github.com>
2023-05-27 12:35:06 -0700

    test(unit-tests): add tests for utils/helpers
    addProps, buildArcStringKey, buildPath

---
+++ tests/unit-tests/helpers/buildArcStringKey.test.ts
@@ -0,0 +1,13 @@
+import { describe, expect, it } from 'vitest';
+import { buildArcStringKey } from '../../../src/lib/utils/helpers/buildArcStringKey';
+
+describe('Testing buildArcStringKey', () => {
+	it('zero values in input coordinates should be represented as "0" in the output string', () => {
+		expect(buildArcStringKey({ x: 0, y: 0 }, { x: 100, y: 100 })).toEqual('0011');
+		expect(buildArcStringKey({ x: 10, y: 10 }, { x: 0, y: 0 })).toEqual('1100');
+	});
+	it('negative values in input coordinates should be represented as "-1" in the output string', () => {
+		expect(buildArcStringKey({ x: 9, y: -10 }, { x: 10, y: -39 })).toEqual('1-11-1');
+		expect(buildArcStringKey({ x: -10, y: -10 }, { x: -100, y: -100 })).toEqual('-1-1-1-1');
+	});
+});
