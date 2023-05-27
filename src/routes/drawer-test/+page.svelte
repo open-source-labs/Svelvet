4adadacf9dbd316f0f1d8cc4bb9b9e205d9d8fed
Welsey-Waters <wesleyswaters@yahoo.com>
2023-05-27 09:48:42 -0700

    chore: remove unused variables and imports

--- src/routes/drawer-test/+page.svelte
+++ src/routes/drawer-test/+page.svelte
@@ -1,28 +1,7 @@
 <script lang="ts">
-	import { Svelvet, Node, Drawer} from '$lib';
-	import InputNode from '../../example-components/InputNode.svelte';
-	import type { NodeConfig } from '$lib/types';
-	import { createNode } from '$lib/utils';
-
-	
+	import { Drawer } from '$lib';
 </script>
 
 <Drawer>
-	<Svelte>
-		{...nodes}
-		{...customNodes}
-	</Svelte>
+	
 </Drawer>
-
-
-
-
-
-<style>
-	body {
-		width: 100vw;
-		height: 100vh;
-		margin: 0;
-		padding: 0;
-	}
-</style>
