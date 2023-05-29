11b12492321c2e83b0ee2e5de0660cb27ab6ec84
Welsey-Waters <wesleyswaters@yahoo.com>
2023-05-28 20:32:50 -0700

    src/lib/components/Drawer/DrawerEdgeCreator.svelte
    Rename file

---
+++ src/lib/components/Drawer/DrawerCustomNodeEdge.svelte
@@ -0,0 +1,59 @@
+<script lang='ts'>
+  import { Edge } from "$lib";
+	import Page from "../../../routes/+page.svelte";
+  import { edgeNodeProps } from "./DrawerEdge.svelte";
+  // import { writable } from "svelte/store";
+  import { beforeUpdate, onMount, setContext } from "svelte";
+  // import type { Graph } from "$lib/types";
+	// import type { NavigationType } from "@sveltejs/kit";
+  // import { getContext } from "svelte";
+  // import { edgeComponentStore } from "./DrawerEdge.svelte";
+	// import { createNode } from "$lib/utils";
+
+  // const graph = getContext<Graph>('graph');
+
+  // const edges = graph.edges;  
+
+
+  // const addToComponentStore = (edge: any) => {
+  //   edgeComponentStore.update(edges => {
+  //     console.log(edges)
+  //     return [...edge]
+  //   })
+  // }
+
+  // // onMount(()=> {
+  // //   console.log('component', edgeComponent)
+  // //   addToComponentStore(edgeComponent)
+  // // })
+
+  // onMount(()=> {
+  //   const htmlEDGE = document.getElementsByClassName('edge')
+  //   const htmlSVG = document.getElementsByClassName('edges-wrapper')
+  //   console.log('htmlEDGE', htmlEDGE)
+  //   console.log('htmlSVG', htmlSVG)
+  //   addToComponentStore(htmlEDGE)
+
+  // })
+
+  // beforeUpdate(() => {
+  //   let containers: any = document.getElementsByClassName('edges-wrapper')
+  //   containers = containers[containers.length - 1]
+  //   const contextEdge:any = getContext('edge')
+  //   console.log('bob', containers)
+  //   // edgeComponentStore.update(edges => {
+  //   //   console.log('store', edges)
+  //   //   if (edges.includes(contextEdge.source.node.edge)) return [...edges];
+  //   //   return [...edges, contextEdge.source.node.edge]
+  //   // })
+  //   edgeComponentStore.set(containers);
+  // })
+
+</script>
+
+
+
+<Edge {...$edgeNodeProps}></Edge>
+
+
+
