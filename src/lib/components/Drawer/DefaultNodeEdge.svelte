5363eb8994216d7475a4440b67feb3b892708161
Welsey-Waters <wesleyswaters@yahoo.com>
2023-05-28 20:27:47 -0700

    fix: refactor login of rendering edge for default nodes

---
+++ src/lib/components/Drawer/DefaultNodeEdge.svelte
@@ -0,0 +1,38 @@
+<script lang='ts'>
+  import { Edge } from "$lib";
+  import { edgeNodeProps } from "./DrawerEdge.svelte";
+
+  // const graph = getContext<Graph>('graph');
+
+  // const edges = graph.edges;  
+
+  // onMount(()=> {
+  //   const htmlEDGE = document.getElementsByClassName('edge')
+  //   const htmlSVG = document.getElementsByClassName('edges-wrapper')
+  //   console.log('htmlEDGE', htmlEDGE)
+  //   console.log('htmlSVG', htmlSVG)
+  //   addToComponentStore(htmlEDGE)
+
+  // })
+
+  // beforeUpdate(() => {
+  //   let containers: any = document.getElementsByClassName('edges-wrapper')
+  //   containers = containers[containers.length - 1]
+  //   const contextEdge:any = getContext('edge')
+  //   console.log('bob', containers)
+  //   // edgeComponentStore.update(edges => {
+  //   //   console.log('store', edges)
+  //   //   if (edges.includes(contextEdge.source.node.edge)) return [...edges];
+  //   //   return [...edges, contextEdge.source.node.edge]
+  //   // })
+  //   edgeComponentStore.set(containers);
+  // })
+
+</script>
+
+
+
+<Edge {...$edgeNodeProps}></Edge>
+
+
+
