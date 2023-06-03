dfcd1673d2be157727ffafc2d8735f78f7504188
sam <sam@cps-macbook-pro.lan>
2023-06-02 18:13:32 -0700

    -refactor: created edge types for the drawer
    Co-authored-by: Ruxin Zheng <RuxinZ@users.noreply.github.com>
    Co-authored-by: Rathna Chinthalapalli <rathna-git@users.noreply.github.com>
    Co-authored-by: Wesley-Waters <Wesley-Waters@users.noreply.github.com>

--- src/lib/types/props/edgeProps.ts
+++ src/lib/types/props/edgeProps.ts
@@ -1,14 +1,27 @@
-import type { CSSColorString } from '../general';
+import type { CSSColorString } from '$lib/types';
 
-export interface EdgeProps {
-	width: number;
-	targetColor: CSSColorString;
-	color: CSSColorString;
-	straight: boolean;
-	step: boolean;
-	cornerRadius: number;
-	animate: false;
-	label: string;
-	labelColor: CSSColorString;
-	textColor: CSSColorString;
+export interface EdgeDrawerConfig {
+	width?: number;
+	targetColor?: CSSColorString;
+	color?: CSSColorString;
+	straight?: boolean;
+	step?: boolean;
+	cornerRadius?: number;
+	animate?: false;
+	label?: string;
+	labelColor?: CSSColorString;
+	textColor?: CSSColorString;
 }
+
+export type EdgeProps = [
+	width: number | undefined,
+	targetColor: CSSColorString | undefined,
+	color: CSSColorString | undefined,
+	straight: boolean | undefined,
+	step: boolean | undefined,
+	cornerRadius: number | undefined,
+	animate: boolean | undefined,
+	label: string | undefined,
+	labelColor: CSSColorString | undefined,
+	textColor: CSSColorString | undefined
+];
