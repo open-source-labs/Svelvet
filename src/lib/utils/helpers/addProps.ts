fb5e81212aad874429c54251ee9259c94950508e
sam <sam@cps-macbook-pro.lan>
2023-06-02 19:26:19 -0700

    -refactor: addition of nodeProps type
    Co-authored-by: Ruxin Zheng <RuxinZ@users.noreply.github.com>
    Co-authored-by: Rathna Chinthalapalli <rathna-git@users.noreply.github.com>
    Co-authored-by: Wesley-Waters <Wesley-Waters@users.noreply.github.com>

--- src/lib/utils/helpers/addProps.ts
+++ src/lib/utils/helpers/addProps.ts
@@ -1,7 +1,19 @@
+import type {
+	NodeProps,
+	AnchorProps,
+	EdgeProps,
+	NodeConfig,
+	AnchorDrawerConfig,
+	EdgeDrawerConfig
+} from '$lib/types';
+
 export function addProps(
 	propNames: string[],
-	propValues: any[],
-	propObject: Record<string, any>
+	propValues: NodeProps | AnchorProps | EdgeProps,
+	propObject: Record<string, NodeConfig | AnchorDrawerConfig | EdgeDrawerConfig>
+	// | NodeConfig
+	// | AnchorDrawerConfig
+	// | EdgeDrawerConfig
 ): void {
 	for (let i = 0; i < propNames.length; i++) {
 		if (propValues[i] !== undefined) propObject[propNames[i]] = propValues[i];
