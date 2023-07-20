5878bee77e89d77b04ce41e0a372f529897cc468
Welsey-Waters <wesleyswaters@yahoo.com>
2023-07-19 21:52:24 -0700

    fix propValues type

--- src/lib/utils/helpers/addProps.ts
+++ src/lib/utils/helpers/addProps.ts
@@ -1,20 +1,16 @@
-// import type {
-// 	NodeProps,
-// 	AnchorProps,
-// 	EdgeProps,
-// 	NodeConfig,
-// 	AnchorDrawerConfig,
-// 	EdgeDrawerConfig
-// } from '$lib/types';
-
-import type { ComponentType } from 'svelte';
+import type {
+	NodeProps,
+	AnchorProps,
+	EdgeProps,
+	NodeConfig,
+	AnchorDrawerConfig,
+	EdgeDrawerConfig
+} from '$lib/types';
 
 export function addProps(
 	propNames: string[],
-	propValues: (string | number | boolean | ComponentType | undefined)[],
-	propObject: Record<string, any>
-	// propObject: Record<string, string | number | boolean | undefined>
-	// propObject: NodeConfig | AnchorDrawerConfig | EdgeDrawerConfig
+	propValues: NodeProps | AnchorProps | EdgeProps,
+	propObject: NodeConfig | AnchorDrawerConfig | EdgeDrawerConfig | any
 ): void {
 	for (let i = 0; i < propNames.length; i++) {
 		if (propValues[i] !== undefined) propObject[propNames[i]] = propValues[i];
