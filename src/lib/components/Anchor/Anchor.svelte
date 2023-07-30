41ed5557ee77a8b021edfb845a407873bb99a736
BOJIT <35117353+BOJIT@users.noreply.github.com>
2023-07-30 21:19:09 +0100

    Fix locking for incoming connections

--- src/lib/components/Anchor/Anchor.svelte
+++ src/lib/components/Anchor/Anchor.svelte
@@ -289,9 +289,10 @@
 
 		// If the anchor receiving the event has connections
 		// And it can't have multiple connections
+		// Or the anchor is locked
 		// Then this is an invalid connection
 		// Delete the cursor edge and clear the linking store
-		if ($connectedAnchors?.size && !multiple) {
+		if (locked || ($connectedAnchors?.size && !multiple)) {
 			edgeStore.delete('cursor');
 			if (!e.shiftKey) clearLinking(false);
 			return;
@@ -598,7 +599,7 @@
 	tabindex="0"
 	class:locked
 	title={title || ''}
-	on:mouseenter={() => (hovering = true)}
+	on:mouseenter={() => (hovering = true && !locked)}
 	on:mouseleave={() => (hovering = false)}
 	on:mousedown|stopPropagation|preventDefault={handleClick}
 	on:mouseup|stopPropagation={handleMouseUp}
