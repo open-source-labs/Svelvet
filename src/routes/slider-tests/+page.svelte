3733eef2e3e608726872964f9c0398e555e70e72
John Costello <johncostello@Johns-MacBook-Pro.local>
2023-10-26 21:13:15 -0400

    created test page for slider and rendered slider and node

---
+++ src/routes/slider-tests/+page.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import { Svelvet, Node, Slider, Anchor } from '$lib';
+	import { generateInput, generateOutput } from '$lib';
+
+	type InputStructure = {
+		sliderVal: number;
+	};
+
+	const initialData = {
+		sliderVal: 10
+	};
+
+	const inputs = generateInput(initialData);
+	const processor = (inputs: InputStructure) => inputs.sliderVal;
+	const outputs = generateOutput(inputs, processor);
+	console.log(outputs);
+</script>
+
+<Svelvet minimap controls theme={'dark'} width={800} height={500}>
+	<Node width={200} height={100} useDefaults position={{ x: 100, y: 200 }}>
+		<Slider parameterStore={$inputs.sliderVal} />
+		<Anchor outputStore={outputs} output />
+	</Node>
+
+	<Node id={'displayNode'} useDefaults width={200} height={100} position={{ x: 400, y: 200 }}>
+		<p>{$outputs}</p>
+		<Anchor inputsStore={inputs} input direction={'west'} />
+	</Node>
+</Svelvet>
