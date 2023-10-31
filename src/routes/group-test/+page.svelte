4aef29df0d8e80b4f12d284e2797d6c45a6f5e13
John Costello <johncostello@Johns-MacBook-Pro.local>
2023-10-31 12:00:35 -0400

    adds testing to verify functionality of group component

---
+++ src/routes/group-test/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { Svelvet, Group, Node } from '$lib';
+</script>
+
+<Svelvet minimap controls theme={'dark'} width={800} height={850}>
+	<Group position={{ x: 200, y: 200 }} width={400} height={300} groupName="nodes">
+		<Node position={{ x: 100, y: 100 }} label="Inside" dimensions={{ width: 100, height: 100 }} />
+	</Group>
+	<Node position={{ x: 650, y: 350 }} label="Right" dimensions={{ width: 100, height: 100 }} />
+	<Node position={{ x: 50, y: 350 }} label="Left" dimensions={{ width: 100, height: 100 }} />
+	<Node position={{ x: 350, y: 50 }} label="Top" dimensions={{ width: 100, height: 100 }} />
+	<Node position={{ x: 350, y: 650 }} label="Bottom" dimensions={{ width: 100, height: 100 }} />
+</Svelvet>
