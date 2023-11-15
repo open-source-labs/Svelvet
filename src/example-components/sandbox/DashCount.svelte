24ddbe6d90a6716734bb16d6065c53258110181c
henry.sweat <hi@henrysweat.com>
2023-11-15 14:08:53 -0500

    flowchart-docs and sandbox

---
+++ src/example-components/sandbox/DashCount.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import { generateInput, generateOutput, Slider, Node } from '$lib';
+	import NodeWrapper from './NodeWrapper.svelte';
+
+	type Inputs = {
+		length: number;
+	};
+
+	const initialData = {
+		length: 26
+	};
+	const inputs = generateInput(initialData);
+	const procesor = (inputs: Inputs) => inputs.length;
+	const output = generateOutput(inputs, procesor);
+</script>
+
+<Node useDefaults position={{ x: 40, y: 158 }} let:selected>
+	<NodeWrapper title="Dash Count" outputStore={output} key="dashCount">
+		<div class="node-body">
+			<Slider min={0} fixed={0} max={30} step={1} parameterStore={$inputs.length} />
+		</div>
+	</NodeWrapper>
+</Node>
+
+<style>
+	.node-body {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		gap: 5px;
+	}
+</style>
---
+++ src/routes/sandbox/+page.svelte
@@ -0,0 +1,65 @@
+<script lang="ts">
+	import { Svelvet, ThemeToggle, Group } from '$lib';
+	import Thickness from '../../example-components/sandbox/Thickness.svelte';
+	import Noise from '../../example-components/sandbox/Noise.svelte';
+	import Scale from '../../example-components/sandbox/Scale.svelte';
+	import CircleColor from '../../example-components/sandbox/CircleColor.svelte';
+	import DashCount from '../../example-components/sandbox/DashCount.svelte';
+	import Output from '../../example-components/sandbox/Output.svelte';
+
+	let zoom = 0.5;
+</script>
+
+<body>
+	<Svelvet edgeStyle="step" TD {zoom} minimap controls>
+		<Group
+			position={{ x: -150, y: -100 }}
+			width={600}
+			height={700}
+			color="goldenrod"
+			groupName="parameters"
+		>
+			<Thickness />
+			<Noise />
+			<Scale />
+			<CircleColor />
+			<DashCount />
+		</Group>
+		<Output />
+		<span id="state" class="note"> Stateful Anchors</span>
+		<span id="groups" class="note">Group Boxes</span>
+		<ThemeToggle main="dark" alt="light" slot="toggle" />
+	</Svelvet>
+</body>
+
+<style>
+	@import url('https://fonts.googleapis.com/css2?family=Reenie+Beanie&display=swap');
+	@import url('https://fonts.googleapis.com/css2?family=Rubik&display=swap');
+	body {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		background-color: gray;
+		width: 100vw;
+		height: 100vh;
+		padding: 0;
+		margin: 0;
+	}
+	.note {
+		font-family: 'Reenie Beanie', sans-serif;
+		position: absolute;
+		color: inherit;
+		width: 400px;
+		transform: rotate(-3deg);
+		font-weight: 200px;
+		font-size: 40px;
+	}
+	#state {
+		top: -30px;
+		left: 620px;
+	}
+	#groups {
+		top: 530px;
+		left: 490px;
+	}
+</style>
