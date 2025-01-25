23a6f6ee01bb7f0bc1a04c6320fd3db8cba885d6
Johann <149050251+jknohr@users.noreply.github.com>
2025-01-25 17:36:58 +0700

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -7,6 +7,7 @@ const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
+		typescript: true,
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
